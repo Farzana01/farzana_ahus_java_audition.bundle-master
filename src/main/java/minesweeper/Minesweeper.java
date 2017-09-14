5a846b677407dd0caa3ded1099e10a687541df7f
Farzana01 <farzana.n.azad@gmail.com>
2017-09-14 15:40:39 -0400

    inserted test class

--- src/main/java/minesweeper/Minesweeper.java
+++ src/main/java/minesweeper/Minesweeper.java
@@ -1,47 +1,37 @@
 package minesweeper;
 
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
-
 public class Minesweeper {
+	
+	int n = 5;
+	int m = 5;
+	String [][] board = new String[n][m];
+	String mine = "*";
+	
+	
+	public void build() {
+		 for (int i = 0; i < board.length; i++) {
+		        for (int j = 0; j < board[0].length; j++) {
+		        	board[i][j] = ".";
+		 System.out.print(board[i][j] + " ");
+		}
+		        System.out.println("");
+		 }
+	}
+	
 
-	public static void main(String[] args) {
-	    int n = 5;
-	    int m = 5;
-	  
-	    
-	    int mines = 5;
-	    
-	    String [][] board = new String[n][m];
-	    
-	    while(mines != 0) {
-	    	 int randRow = (int)(Math.random()*n);
-		     int randCol = (int)(Math.random()*m);
-		     
-	    for (int i = 0; i < board.length; i++) {
-	        for (int j = 0; j < board[0].length; j++) {
-	        	if(mines>0 && i==randRow && j==randCol) {
-	        		board[i][j] = "*";
-	        		mines--;
-	        		randRow = (int)(Math.random()*n);
-	    		    randCol = (int)(Math.random()*m);
-	        	} else {
-	                     board[i][j] = " . ";
-
-	        	}
-	        }
-	    }
-	    
-	    }
-	    //prints the game board
-	    for (int i = 0; i < board.length; i++) {
-	        for (int j = 0; j < board[0].length; j++) {
-	       
-	           System.out.print(board[i][j] + " ");
-	        }
-	        System.out.println("");
-	    }
+	public void addMines(int numMines) {
+		 for (int i = 0; i < board.length; i++) {
+		        for (int j = 0; j < board[0].length; j++) {
+		       	 int randRow = (int)(Math.random()*numMines);
+			     int randCol = (int)(Math.random()*numMines);
+		        	if( i== randRow && j == randCol) {
+		        		board[i][j] = "*";
+		        			} else {
+		                     board[i][j] = " . ";
+		        }
+		        	
+		        }
+		}
+	
 	}
 }
-
--- src/test/java/minesweeper/MinesweeperTest.java
+++ src/test/java/minesweeper/MinesweeperTest.java
@@ -1,4 +1,40 @@
 
+package minesweeper;
 
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class MinesweeperTest {
+	 	
+	private Minesweeper game;
+
+	@Before
+	public void setup() {
+		game = new Minesweeper();
+	}
+	
+	@Test
+	public void shouldSay0Bombs() {
+		System.out.println("\nField # 1:");
+		game.build();
+		game.addMines(0);
+		assertEquals(0, 0);
+	}
+	
+	@Test
+	public void shouldSay1Bombs() {
+		System.out.println("\nField # 2:");
+		game.build();
+		game.addMines(1);
+		assertEquals(1, 1); //not a correct test doesn't account for adjacent cells
+	}
+}
+	
+
+	
+
+	
+	
 	
